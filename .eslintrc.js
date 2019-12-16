e148ab170c1f553265881f71ed0c7cfe90c83dd2
Marcos Guedes <mguedes@growin.pt>
2019-12-16 10:51:13 +0000

    get and post with mongodb data working

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": "airbnb",
+    rules: {
+        "linebreak-style": ["error", "windows"],
+        "no-console": ["error", { 
+            allow: ["log", "warn", "error"] 
+        }],
+        "comma-dangle": 0
+    }
 };
--- app.js
+++ app.js
@@ -1,12 +1,51 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const db = mongoose.connect('mongodb://localhost:27017/DbTest')
+const Book = require('./models/bookModel');
+
+const bookRouter = express.Router();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+bookRouter.route('/books')
+  .post((req, res) => {
+    const book = new Book(req.body);
+    
+    book.save();
+    return res.status(201).json(book);
+  })
+  .get((req, res) => {
+    const { query } = req;
+    Book.find(query, (err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    })
+  });
+
+bookRouter.route('/books/:bookId')
+  .get((req, res) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(book);
+    })
+  });
+
+app.use('/api', bookRouter);
+
 app.get('/', (req, res) => {
-    res.send('Welcome to my API!');
+  res.send('Welcome to my API!');
 });
 
 app.listen(port, () => {
-    console.log(`Running on port ${port}`);
+  console.log(`Running on port ${port}`);
 });
